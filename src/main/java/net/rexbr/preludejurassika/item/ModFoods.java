c26d8614f04ca0a67c1ff2c41ebaa922da60dd7a
rexbr_3615 <83562258+rexbr3615@users.noreply.github.com>
2022-09-17 18:29:18 -0300

    loot

---
+++ src/main/java/net/rexbr/preludejurassika/item/ModFoods.java
@@ -0,0 +1,7 @@
+package net.rexbr.preludejurassika.item;
+
+import net.minecraft.world.food.FoodProperties;
+
+public class ModFoods {
+    public static final FoodProperties DODO_RAW = (new FoodProperties.Builder()).fast().nutrition(1).saturationMod(0.1F).build();
+}
--- src/main/java/net/rexbr/preludejurassika/item/ModItems.java
+++ src/main/java/net/rexbr/preludejurassika/item/ModItems.java
@@ -11,8 +11,22 @@ public class ModItems {
     public static final DeferredRegister<Item> ITEMS =
             DeferredRegister.create(ForgeRegistries.ITEMS, prelude.MODID);
 
+    // items
     public static final RegistryObject<Item> FOSSIL = ITEMS.register("fossil",
             () -> new Item(new Item.Properties().tab(ModCreativeModeTab.FOSSIL_MOD_ITEMS)));
+    //foods
+    public static final RegistryObject<Item> DODO_RAW = ITEMS.register("raw_dodo",
+            () -> new Item(new Item.Properties().tab(ModCreativeModeTab.FOSSIL_MOD_ITEMS).food(ModFoods.DODO_RAW)));
+
+    //eggs
+
+    //spawn eggs
+
+    //dna
+    public static final RegistryObject<Item> DODO_DNA = ITEMS.register("dodo_dna",
+            () -> new Item(new Item.Properties().tab(ModCreativeModeTab.FOSSIL_MOD_ITEMS).stacksTo(1)));
+    public static final RegistryObject<Item> DODO_SYRINGUE = ITEMS.register("dodo_syringue",
+            () -> new Item(new Item.Properties().tab(ModCreativeModeTab.FOSSIL_MOD_ITEMS).stacksTo(1)));
 
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
