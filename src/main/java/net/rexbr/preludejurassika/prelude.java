4d71a736d83b14d65317ae6d30f062cca4f1a12b
rexbr_3615 <83562258+rexbr3615@users.noreply.github.com>
2022-09-17 15:25:57 -0300

    Initial commit

---
+++ src/main/java/net/rexbr/preludejurassika/prelude.java
@@ -0,0 +1,32 @@
+package net.rexbr.preludejurassika;
+
+import com.mojang.logging.LogUtils;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.slf4j.Logger;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod(prelude.MODID)
+public class prelude {
+
+    public static final String MODID = "prelude";
+
+    private static final Logger LOGGER = LogUtils.getLogger();
+
+    public prelude() {
+        // Register the setup method for modloading
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+
+        // Register ourselves for server and other game events we are interested in
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void setup(final FMLCommonSetupEvent event) {
+        // some preinit code
+        LOGGER.info("HELLO FROM PREINIT");
+        LOGGER.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
+    }
+}
