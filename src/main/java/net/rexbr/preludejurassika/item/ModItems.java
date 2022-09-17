11086d733cb06549cad6c26f00e2784be541f6dd
rexbr_3615 <83562258+rexbr3615@users.noreply.github.com>
2022-09-17 18:56:22 -0300

    correction of json lang file

--- src/main/java/net/rexbr/preludejurassika/item/ModItems.java
+++ src/main/java/net/rexbr/preludejurassika/item/ModItems.java
@@ -1,6 +1,6 @@
 package net.rexbr.preludejurassika.item;
 
-import net.minecraft.world.item.Item;
+import net.minecraft.world.item.*;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -27,6 +27,24 @@ public class ModItems {
             () -> new Item(new Item.Properties().tab(ModCreativeModeTab.FOSSIL_MOD_ITEMS).stacksTo(1)));
     public static final RegistryObject<Item> DODO_SYRINGUE = ITEMS.register("dodo_syringue",
             () -> new Item(new Item.Properties().tab(ModCreativeModeTab.FOSSIL_MOD_ITEMS).stacksTo(1)));
+
+    //weapons
+    public static final RegistryObject<Item> FOSSIL_SWORD = ITEMS.register("fossil_sword",
+            () -> new SwordItem(ModTiers.FOSSIL, 2, 3f,
+                    new Item.Properties().tab(ModCreativeModeTab.FOSSIL_MOD_ITEMS).durability(900)));
+    public static final RegistryObject<Item> FOSSIL_PICKAXE = ITEMS.register("fossil_pickaxe",
+            () -> new PickaxeItem(ModTiers.FOSSIL, 1, 1f,
+                    new Item.Properties().tab(ModCreativeModeTab.FOSSIL_MOD_ITEMS).durability(900)));
+    public static final RegistryObject<Item> FOSSIL_SHOVEL = ITEMS.register("fossil_shovel",
+            () -> new ShovelItem(ModTiers.FOSSIL, 0, 1f,
+                    new Item.Properties().tab(ModCreativeModeTab.FOSSIL_MOD_ITEMS).durability(900)));
+    public static final RegistryObject<Item> FOSSIL_AXE = ITEMS.register("fossil_axe",
+            () -> new AxeItem(ModTiers.FOSSIL, 4, 0f,
+                    new Item.Properties().tab(ModCreativeModeTab.FOSSIL_MOD_ITEMS).durability(900)));
+    public static final RegistryObject<Item> FOSSIL_HOE = ITEMS.register("fossil_hoe",
+            () -> new HoeItem(ModTiers.FOSSIL, 0, 0f,
+                    new Item.Properties().tab(ModCreativeModeTab.FOSSIL_MOD_ITEMS).durability(900)));
+
 
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
