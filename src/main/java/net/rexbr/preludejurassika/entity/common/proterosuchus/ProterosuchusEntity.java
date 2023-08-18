c5fad611fe4a65d308bd644f02f3adc38a9ce61a
rexbr_3615 <83562258+rexbr3615@users.noreply.github.com>
2023-08-18 14:34:21 -0300

    Atualizar o ProterosuchusEntity.java

--- src/main/java/net/rexbr/preludejurassika/entity/common/proterosuchus/ProterosuchusEntity.java
+++ src/main/java/net/rexbr/preludejurassika/entity/common/proterosuchus/ProterosuchusEntity.java
@@ -36,7 +36,7 @@ public class ProterosuchusEntity extends Animal implements IAnimatable {
 
     public static AttributeSupplier setAttributes() {
         return Animal.createMobAttributes()
-                .add(Attributes.MAX_HEALTH, 27.0D)
+                .add(Attributes.MAX_HEALTH, 26.0D)
                 .add(Attributes.ATTACK_DAMAGE, 6f)
                 .add(Attributes.ATTACK_SPEED, 2.0f)
                 .add(Attributes.MOVEMENT_SPEED, 0.24f).build();
